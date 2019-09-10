a9b7c36d5010b19c5f3dc951c6b8c66f6167b1b2
Dylan Ramirez Hernandez <dylanramirez2000@ciencias.unam.mx>
2019-09-10 14:59:04 -0500

    se agregaron pelota01.py y pelota02.py

---
+++ Python/pelota01.py
@@ -0,0 +1,15 @@
+# 
+# Dylan Ramirez Hernandez
+# python 3.7.3
+# 10/09/2019, 16/09/2019
+# 12/09/2019, 17/09/2019
+#
+# Calcula la posicion de una pelota con una velocidad inicial
+# de 5 m/s y un tiempo de 0.6 segundos
+v0 = 5
+g = 9.81
+t = 0.6
+y = v0*t - 0.5*g*t**2
+print(y)
+    
+
